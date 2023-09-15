b45cc99bedc66492f5c63960ce5ed3451c78b570
Alin Triff <triffalin@gmail.com>
2023-09-15 09:53:19 +0300

    added another selectors for ClassName and ByTagName

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 // document.body.children[1].children[0].href = 'https://google.com';
 
 //Queryng elements
-// getElementById - query for element trough ID
+// getElementById('some-id") - Select a HTML element by its ID (selects only one element, since IDs should be unique)
 let anchorElement = document.getElementById('external-link');
 anchorElement.href = 'https://google.com';
 
-// querySelector - like CSS selector, but matching  just first element
+// querySelector('<some-css-selector>') - Selects the first matching (!) HTML element that is met / selected by the provided CSS selector; The CSS selector can basically be any kind of valid CSS selector (e.g. ID selector, tag type selector, class selector, combined selectors etc.)
 anchorElement = document.querySelector('a'); // a { color: red; }
 anchorElement.href = 'https://academind.com';
 
-//querySelectorAll - matches all elements
+//querySelectorAll('<some-css-selector>') - Selects ALL matching HTML elements that are met / selected by the provided CSS selector
+
+// document.getElementsByClassName('some-css-class')- Selects all HTML elements that have the provided CSS class
+
+// document.getElementsByTagName('tag') - Selects all HTML elements that are of the provided HTML tag type
