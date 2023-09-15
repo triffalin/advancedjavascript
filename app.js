95d5ad421ee4f2af4cc1347e34b387dad6c0d32f
Alin Triff <triffalin@gmail.com>
2023-09-15 09:48:48 +0300

    queried elements trough querySelector/querySelectorAll/getElementById

--- app.js
+++ app.js
@@ -2,5 +2,12 @@
 // document.body.children[1].children[0].href = 'https://google.com';
 
 //Queryng elements
+// getElementById - query for element trough ID
 let anchorElement = document.getElementById('external-link');
 anchorElement.href = 'https://google.com';
+
+// querySelector - like CSS selector, but matching  just first element
+anchorElement = document.querySelector('a'); // a { color: red; }
+anchorElement.href = 'https://academind.com';
+
+//querySelectorAll - matches all elements
