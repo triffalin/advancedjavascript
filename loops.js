152a2d6c0bab94aff13555969b631a67a3ccd92a
Alin Triff <triffalin@gmail.com>
2023-09-17 18:36:36 +0300

    introduced the while loop

--- loops.js
+++ loops.js
@@ -19,3 +19,11 @@ for (const propertyName in loggedInUser) {
 	console.log(loggedInUser.name);
 	console.log(loggedInUser[propertyName]);
 }
+
+let isFinished = false;
+
+while (!isFinished) {
+	isFinished = confirm('Do you want to quit?');
+}
+
+console.log('Done!');
