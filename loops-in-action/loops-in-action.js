304949fad81ec3e8791fc8f7def4db24e031cb02
Alin Triff <triffalin@gmail.com>
2023-09-17 19:59:04 +0300

    finished the third exercise display user data

--- loops-in-action/loops-in-action.js
+++ loops-in-action/loops-in-action.js
@@ -35,3 +35,29 @@ function highlightLinks() {
 }
 
 highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+// Display your information
+
+const dummyUserData = {
+	firstName: 'Alin',
+	lastName: 'Triff',
+	age: 32,
+};
+
+const displayUserDataButtonElement =
+	document.querySelector('#user-data button');
+
+function displayUserData() {
+	const outputDataElement = document.getElementById('output-user-data');
+
+	outputDataElement.innerHTML = '';
+
+	for (const key in dummyUserData) {
+		const newUserDataListItemElement = document.createElement('li');
+		const outputText = key.toUpperCase() + ': ' + dummyUserData[key];
+		newUserDataListItemElement.textContent = outputText;
+		outputDataElement.append(newUserDataListItemElement);
+	}
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData);
