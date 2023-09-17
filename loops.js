86caa104d4410dfb50234c2ebcca99f4aa41c064
Alin Triff <triffalin@gmail.com>
2023-09-17 18:31:56 +0300

    introduced the for-in loop (for object)

--- loops.js
+++ loops.js
@@ -5,5 +5,17 @@ for (let i = 0; i < 10; i++) {
 const users = ['Max', 'Anna', 'Joel'];
 
 for (const user of users) {
-	console.log(users);
+	console.log(user);
+}
+
+const loggedInUser = {
+	name: 'Alin',
+	age: 32,
+	isAdmin: true,
+};
+
+for (const propertyName in loggedInUser) {
+	console.log(propertyName);
+	console.log(loggedInUser.name);
+	console.log(loggedInUser[propertyName]);
 }
