de7a3958b5e3cb9188eb4bee9b005ca0cab591f5
Alin Triff <triffalin@gmail.com>
2023-09-17 17:37:13 +0300

    extended the boolean values

--- conditional-code.js
+++ conditional-code.js
@@ -3,3 +3,13 @@ const myName = 'Alin';
 if (myName === 'Alin') {
 	console.log('Hello!');
 }
+
+let isLoggedIn = true;
+
+// if (isLoggedIn) {
+// 	console.log('User is logged in!');
+// }
+
+if (!isLoggedIn) {
+	console.log('User is NOT logged in');
+}
